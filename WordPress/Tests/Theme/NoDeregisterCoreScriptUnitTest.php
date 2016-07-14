5e7d40de7098742c52e25774ddd337610bd2a461
Simon Prosser <simon@pagelines.com>
2016-07-14 17:15:07 +0100

    New "NoDeregisterCoreScript" sniff
    Includes:
    * Move brackets
    * Make in_array strict
    * Remove unneeded tests
    * Remove unneeded tests from errorlist

---
+++ WordPress/Tests/Theme/NoDeregisterCoreScriptUnitTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  Theme
+ * @package   WordPress_Coding_Standards
+ * @author    Simon Prosser <pross@pross.org.uk>
+ */
+class WordPress_Tests_Theme_NoDeregisterCoreScriptUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getErrorList() {
+		return array(
+			6 => 1,
+			8 => 1,
+			10 => 1,
+			12 => 1,
+		);
+	}//end getErrorList()
+
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of warnings that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getWarningList() {
+		return array();
+	}//end getWarningList()
+}//end class
