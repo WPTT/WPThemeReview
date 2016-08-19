ce4dc5df168247ecbc774e6b0c95231ec48163a0
khacoder <kevin@kevinsspace.ca>
2016-08-18 20:30:58 -0600

    Initial Commit
    This is the first sniff for add_theme_support and tags.
    Finds add_theme_support() declarations and checks tags.
    Requires WordPress AbstractThemeSniff class.

---
+++ WordPress/Tests/Theme/AddThemeSupportTagsCheck1UnitTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Unit test class for WordPress Coding Standard.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     https://make.wordpress.org/core/handbook/best-practices/coding-standards/
+ */
+
+/**
+ * WordPress_Tests_Theme_AddThemeSupportTagsCheckUnitTest
+ *
+ * Verify that an add_theme_support() call is made for any feature the
+ * theme has been tagged with from the following list: custom-background,
+ * custom-header, custom-menu, featured-images/post-thumbnails, post-formats,
+ * custom-logo.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   khacoder
+ */
+class WordPress_Tests_Theme_AddThemeSupportTagsCheck1UnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getErrorList() {
+		return array();
+
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of warnings that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getWarningList() {
+		return array();
+
+	} // end getWarningList()
+
+} // end class
