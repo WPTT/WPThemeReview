c62c8905f2e5885f81b60594bafd48a3e02e3c7c
khacoder <kevin@kevinsspace.ca>
2016-08-29 18:57:10 -0600

    Search Form Direct Load
    Initial Commit
    Issue #74

---
+++ WordPress/Tests/Theme/SearchFormUnitTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Unit test class for the Theme_SearchForm sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category Theme
+ * @package  PHP_CodeSniffer
+ * @author	 khacoder
+ */
+class WordPress_Tests_Theme_SearchFormUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getErrorList() {
+		return array(
+			2 => 1,
+		);
+
+	} // end getErrorList()
+
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of warnings that should occur on that line.
+	 *
+	 * @return array(int => int)
+	 */
+	public function getWarningList() {
+		return array();
+
+	} // end getWarningList()
+
+} // end class
