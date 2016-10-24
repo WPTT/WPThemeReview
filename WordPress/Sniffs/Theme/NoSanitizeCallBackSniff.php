440bac360f68fc5f91adbb5e859329557eb79930
Ulrich Pogson <grapplerulrich@gmail.com>
2016-10-24 06:23:24 +0200

    Use utility functions & add more test examples

--- WordPress/Sniffs/Theme/NoSanitizeCallBackSniff.php
+++ WordPress/Sniffs/Theme/NoSanitizeCallBackSniff.php
@@ -26,10 +26,7 @@ class WordPress_Sniffs_Theme_NoSanitizeCallbackSniff implements PHP_CodeSniffer_
 	public function register() {
 		return array(
 			T_STRING,
-			T_CONSTANT_ENCAPSED_STRING,
-			T_VARIABLE,
-			T_LNUMBER,
-			T_SEMICOLON,
+			T_EVAL,
 		);
 	}
 
@@ -46,49 +43,266 @@ class WordPress_Sniffs_Theme_NoSanitizeCallbackSniff implements PHP_CodeSniffer_
 
 		$tokens = $phpcsFile->getTokens();
 		$token  = $tokens[ $stackPtr ];
-		$types  = array( T_STRING, T_CONSTANT_ENCAPSED_STRING, T_VARIABLE, T_LNUMBER, T_SEMICOLON );
 
-		if ( '$wp_customize' === $token['content'] ) {
-			$nextStackPtr = $phpcsFile->findNext( $types , $stackPtr + 1 );
-			if ( 'add_setting' === $tokens[ $nextStackPtr ]['content'] ) {
-				$sanitize_callback_found = false;
-				$sanitize_js_callback_found = false;
-				$empty_callback = true;
-				$empty_js_callback = true;
-				while ( ';' !== $tokens[ $nextStackPtr ]['content'] ) {
-					$nextStackPtr = $phpcsFile->findNext( $types , $nextStackPtr + 1 );
-					if ( 'sanitize_callback' === trim( $tokens[ $nextStackPtr ]['content'], '\'\"' ) ) {
-						$sanitize_callback_found = true;
-						$nextnextStackPtr = $phpcsFile->findNext( $types , $nextStackPtr + 1 );
-						if ( '' === trim( $tokens[ $nextnextStackPtr ]['content'], '\'\"' ) ) {
-							$empty_callback = true;
-						} else {
-							$empty_callback = false;
-						}
-					}
-					if ( 'sanitize_js_callback' === trim( $tokens[ $nextStackPtr ]['content'], '\'\"' ) ) {
-						$sanitize_js_callback_found = true;
-						$nextnextStackPtr = $phpcsFile->findNext( $types , $nextStackPtr + 1 );
-						if ( '' === trim( $tokens[ $nextnextStackPtr ]['content'], '\'\"' ) ) {
-							$empty_js_callback = true;
-						} else {
-							$empty_js_callback = false;
-						}
-					}
-				}
-				if ( false === $sanitize_callback_found && false === $sanitize_js_callback_found ) {
+		// Exclude function definitions, static class methods, and namespaced calls.
+		if (
+			T_STRING === $token['code']
+			&&
+			( $prev = $phpcsFile->findPrevious( T_WHITESPACE, ( $stackPtr - 1 ), null, true ) )
+			&&
+			// Skip sniffing if calling anything but class method.
+			( T_OBJECT_OPERATOR !== $tokens[ $prev ]['code'] )
+			||
+			// Skip namespaced functions, ie: \foo\bar() not \bar().
+			(
+				T_NS_SEPARATOR === $tokens[ $prev ]['code']
+				&&
+				( $pprev = $phpcsFile->findPrevious( T_WHITESPACE, ( $prev - 1 ), null, true ) )
+				&&
+				T_STRING === $tokens[ $pprev ]['code']
+			)
+			) {
+			return;
+		}
+
+		if ( $token['content'] !== 'add_setting') {
+			return;
+		}
+
+		$parameter_arg = $this->getFunctionCallParameter( $phpcsFile, $stackPtr, 2 );
+
+		$sanitize_callback_found    = false;
+		$sanitize_js_callback_found = false;
+
+		$sanitize_callback_key = $parameter_arg['start'] - 1;
+
+		while ( $sanitize_callback_key = $phpcsFile->findNext( array( T_CONSTANT_ENCAPSED_STRING ), $sanitize_callback_key + 1, $parameter_arg['end'], false ) ) {
+
+			$name = trim( $tokens[ $sanitize_callback_key ]['content'], '\'\"' );
+
+			if ( 'sanitize_callback' === $name ) {
+				$sanitize_callback_found = true;
+			}
+			if ( 'sanitize_js_callback' === $name ) {
+				$sanitize_js_callback_found = true;
+			}
+
+			if ( false === $sanitize_js_callback_found && false == $sanitize_callback_found ) {
+				$last = $phpcsFile->findPrevious( array( T_CONSTANT_ENCAPSED_STRING ), $parameter_arg['end'] );
+				if ( $last === $sanitize_callback_key ) {
 					$phpcsFile->addError( 'Neither sanitize_callback or sanitize_js_callback were found for this option.', $stackPtr, 'SanitizeCallbackChecks' );
 				}
-				if ( false !== $sanitize_callback_found && false !== $empty_callback && true === $sanitize_js_callback_found && true === $empty_js_callback ) {
-					$phpcsFile->addError( 'Either sanitize_callback or sanitize_js_callback must not be empty.', $stackPtr, 'SanitizeCallbackChecks' );
-				}
-				if ( false === $sanitize_callback_found && false !== $sanitize_js_callback_found ) {
-					$phpcsFile->addWarning( 'Found sanitize_js_callback but not sanitize_callback - is that intended?', $stackPtr, 'SanitizeCallbackChecks' );
-				}
-				if ( true === $sanitize_callback_found && true === $empty_callback && false === $empty_js_callback ) {
-					$phpcsFile->addWarning( 'Found sanitize_js_callback not empty and sanitize_callback is empty - is that intended?', $stackPtr, 'SanitizeCallbackChecks' );
-				}
+				continue;
 			}
+
+			$sanitize_callback = $phpcsFile->findNext( array( T_CONSTANT_ENCAPSED_STRING ), $sanitize_callback_key + 1 );
+			$value = trim( $tokens[ $sanitize_callback ]['content'], '\'\"' );
+
+			if ( ! empty( $value ) ) {
+				continue;
+			}
+
+			$phpcsFile->addError(
+				'The %s must not be empty.',
+				$sanitize_callback,
+				'SanitizeCallbackChecks',
+				array( $name )
+			);
+
+		}
+
+	}
+
+
+	/**
+	 * Checks if a function call has parameters.
+	 *
+	 * Expects to be passed the T_STRING stack pointer for the function call.
+	 * If passed a T_STRING which is *not* a function call, the behaviour is unreliable.
+	 *
+	 * @link https://github.com/wimg/PHPCompatibility/issues/120
+	 * @link https://github.com/wimg/PHPCompatibility/issues/152
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+	 * @param int                  $stackPtr  The position of the function call token.
+	 *
+	 * @return bool
+	 */
+	public function doesFunctionCallHaveParameters(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+	{
+		$tokens = $phpcsFile->getTokens();
+
+		// Check for the existence of the token.
+		if (isset($tokens[$stackPtr]) === false) {
+			return false;
+		}
+
+		if ($tokens[$stackPtr]['code'] !== T_STRING) {
+			return false;
+		}
+
+		// Next non-empty token should be the open parenthesis.
+		$openParenthesis = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, $stackPtr + 1, null, true, null, true);
+		if ($openParenthesis === false || $tokens[$openParenthesis]['code'] !== T_OPEN_PARENTHESIS) {
+			return false;
+		}
+
+		if (isset($tokens[$openParenthesis]['parenthesis_closer']) === false) {
+			return false;
+		}
+
+		$closeParenthesis = $tokens[$openParenthesis]['parenthesis_closer'];
+		$nextNonEmpty     = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, $openParenthesis + 1, $closeParenthesis + 1, true);
+
+		if ($nextNonEmpty === $closeParenthesis) {
+			// No parameters.
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Count the number of parameters a function call has been passed.
+	 *
+	 * Expects to be passed the T_STRING stack pointer for the function call.
+	 * If passed a T_STRING which is *not* a function call, the behaviour is unreliable.
+	 *
+	 * @link https://github.com/wimg/PHPCompatibility/issues/111
+	 * @link https://github.com/wimg/PHPCompatibility/issues/114
+	 * @link https://github.com/wimg/PHPCompatibility/issues/151
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+	 * @param int                  $stackPtr  The position of the function call token.
+	 *
+	 * @return int
+	 */
+	public function getFunctionCallParameterCount(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+	{
+		if ($this->doesFunctionCallHaveParameters($phpcsFile, $stackPtr) === false) {
+			return 0;
+		}
+
+		return count($this->getFunctionCallParameters($phpcsFile, $stackPtr));
+	}
+
+
+	/**
+	 * Get information on all parameters passed to a function call.
+	 *
+	 * Expects to be passed the T_STRING stack pointer for the function call.
+	 * If passed a T_STRING which is *not* a function call, the behaviour is unreliable.
+	 *
+	 * Will return an multi-dimentional array with the start token pointer, end token
+	 * pointer and raw parameter value for all parameters. Index will be 1-based.
+	 * If no parameters are found, will return an empty array.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile     The file being scanned.
+	 * @param int                  $stackPtr      The position of the function call token.
+	 *
+	 * @return array
+	 */
+	public function getFunctionCallParameters(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+	{
+		if ($this->doesFunctionCallHaveParameters($phpcsFile, $stackPtr) === false) {
+			return array();
+		}
+
+		// Ok, we know we have a T_STRING with parameters and valid open & close parenthesis.
+		$tokens = $phpcsFile->getTokens();
+
+		$openParenthesis  = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, $stackPtr + 1, null, true, null, true);
+		$closeParenthesis = $tokens[$openParenthesis]['parenthesis_closer'];
+
+		// Which nesting level is the one we are interested in ?
+		$nestedParenthesisCount = 1;
+		if (isset($tokens[$openParenthesis]['nested_parenthesis'])) {
+			$nestedParenthesisCount = count($tokens[$openParenthesis]['nested_parenthesis']) + 1;
+		}
+
+		$parameters = array();
+		$nextComma  = $openParenthesis;
+		$paramStart = $openParenthesis + 1;
+		$cnt        = 1;
+		while ($nextComma = $phpcsFile->findNext(array(T_COMMA, T_CLOSE_PARENTHESIS, T_OPEN_SHORT_ARRAY), $nextComma + 1, $closeParenthesis + 1)) {
+			// Ignore anything within short array definition brackets.
+			if (
+				$tokens[$nextComma]['type'] === 'T_OPEN_SHORT_ARRAY'
+				&&
+				( isset($tokens[$nextComma]['bracket_opener']) && $tokens[$nextComma]['bracket_opener'] === $nextComma )
+				&&
+				isset($tokens[$nextComma]['bracket_closer'])
+			) {
+				// Skip forward to the end of the short array definition.
+				$nextComma = $tokens[$nextComma]['bracket_closer'];
+				continue;
+			}
+
+			// Ignore comma's at a lower nesting level.
+			if (
+				$tokens[$nextComma]['type'] === 'T_COMMA'
+				&&
+				isset($tokens[$nextComma]['nested_parenthesis'])
+				&&
+				count($tokens[$nextComma]['nested_parenthesis']) !== $nestedParenthesisCount
+			) {
+				continue;
+			}
+
+			// Ignore closing parenthesis if not 'ours'.
+			if ($tokens[$nextComma]['type'] === 'T_CLOSE_PARENTHESIS' && $nextComma !== $closeParenthesis) {
+				continue;
+			}
+
+			// Ok, we've reached the end of the parameter.
+			$parameters[$cnt]['start'] = $paramStart;
+			$parameters[$cnt]['end']   = $nextComma - 1;
+			$parameters[$cnt]['raw']   = trim($phpcsFile->getTokensAsString($paramStart, ($nextComma - $paramStart)));
+
+			// Check if there are more tokens before the closing parenthesis.
+			// Prevents code like the following from setting a third parameter:
+			// functionCall( $param1, $param2, );
+			$hasNextParam = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, $nextComma + 1, $closeParenthesis, true, null, true);
+			if ($hasNextParam === false) {
+				break;
+			}
+
+			// Prepare for the next parameter.
+			$paramStart = $nextComma + 1;
+			$cnt++;
+		}
+
+		return $parameters;
+	}
+
+
+	/**
+	 * Get information on a specific parameter passed to a function call.
+	 *
+	 * Expects to be passed the T_STRING stack pointer for the function call.
+	 * If passed a T_STRING which is *not* a function call, the behaviour is unreliable.
+	 *
+	 * Will return a array with the start token pointer, end token pointer and the raw value
+	 * of the parameter at a specific offset.
+	 * If the specified parameter is not found, will return false.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile   The file being scanned.
+	 * @param int                  $stackPtr    The position of the function call token.
+	 * @param int                  $paramOffset The 1-based index position of the parameter to retrieve.
+	 *
+	 * @return array|false
+	 */
+	public function getFunctionCallParameter(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $paramOffset)
+	{
+		$parameters = $this->getFunctionCallParameters($phpcsFile, $stackPtr);
+
+		if (isset($parameters[$paramOffset]) === false) {
+			return false;
+		}
+		else {
+			return $parameters[$paramOffset];
 		}
 	}
+
+
 }
