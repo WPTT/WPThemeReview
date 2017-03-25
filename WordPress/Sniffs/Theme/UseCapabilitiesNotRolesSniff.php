e9f10b329d0fc91a106b4a0feaac3d7b6fadfe6b
Ulrich Pogson <grapplerulrich@gmail.com>
2017-03-25 21:50:11 +0100

    Update lists and messages

--- WordPress/Sniffs/Theme/UseCapabilitiesNotRolesSniff.php
+++ WordPress/Sniffs/Theme/UseCapabilitiesNotRolesSniff.php
@@ -28,32 +28,107 @@ class WordPress_Sniffs_Theme_UseCapabilitiesNotRolesSniff extends WordPress_Abst
 	protected $group_name = 'caps_not_roles';
 
 	/**
-	 * Array of functions with position.
+	 * Array of functions that accept roles and capabilities as an agrument.
 	 *
 	 * The number represents the position in the function call
 	 * passed variables, here the capability is to be listed.
+	 * The list is sorted alphabetically.
 	 *
 	 * @since 0.xx.0
 	 *
 	 * @var array Function name with parameter position.
 	 */
 	protected $target_functions = array(
-		'add_dashboard_page'        => 3,
-		'add_posts_page'            => 3,
-		'add_media_page'            => 3,
-		'add_pages_page'            => 3,
 		'add_comments_page'         => 3,
-		'add_theme_page'            => 3,
-		'add_plugins_page'          => 3,
-		'add_users_page'            => 3,
+		'add_dashboard_page'        => 3,
 		'add_management_page'       => 3,
-		'add_options_page'          => 3,
+		'add_media_page'            => 3,
 		'add_menu_page'             => 3,
-		'add_utility_page'          => 3,
+		'add_object_page'           => 3,
+		'add_options_page'          => 3,
+		'add_pages_page'            => 3,
+		'add_plugins_page'          => 3,
+		'add_posts_page'            => 3,
 		'add_submenu_page'          => 4,
-		'current_user_can'          => 1,
+		'add_theme_page'            => 3,
+		'add_users_page'            => 3,
+		'add_utility_page'          => 3,
 		'author_can'                => 2,
+		'current_user_can'          => 1,
 		'current_user_can_for_blog' => 2,
+		'user_can'                  => 2,
+
+	);
+
+	/**
+	 * Array of core capabilities.
+	 *
+	 * @link https://github.com/WordPress/wordpress-develop/blob/master/tests/phpunit/tests/user/capabilities.php
+	 *
+	 * @since 0.xx.0
+	 *
+	 * @var array Capabilities available in core.
+	 */
+	protected $core_capabilities = array(
+		'unfiltered_html'        => true,
+		'activate_plugins'       => true,
+		'create_users'           => true,
+		'delete_plugins'         => true,
+		'delete_themes'          => true,
+		'delete_users'           => true,
+		'edit_files'             => true,
+		'edit_plugins'           => true,
+		'edit_themes'            => true,
+		'edit_users'             => true,
+		'install_plugins'        => true,
+		'install_themes'         => true,
+		'update_core'            => true,
+		'update_plugins'         => true,
+		'update_themes'          => true,
+		'edit_theme_options'     => true,
+		'export'                 => true,
+		'import'                 => true,
+		'list_users'             => true,
+		'manage_options'         => true,
+		'promote_users'          => true,
+		'remove_users'           => true,
+		'switch_themes'          => true,
+		'edit_dashboard'         => true,
+		'moderate_comments'      => true,
+		'manage_categories'      => true,
+		'edit_others_posts'      => true,
+		'edit_pages'             => true,
+		'edit_others_pages'      => true,
+		'edit_published_pages'   => true,
+		'publish_pages'          => true,
+		'delete_pages'           => true,
+		'delete_others_pages'    => true,
+		'delete_published_pages' => true,
+		'delete_others_posts'    => true,
+		'delete_private_posts'   => true,
+		'edit_private_posts'     => true,
+		'read_private_posts'     => true,
+		'delete_private_pages'   => true,
+		'edit_private_pages'     => true,
+		'read_private_pages'     => true,
+		'edit_published_posts'   => true,
+		'upload_files'           => true,
+		'publish_posts'          => true,
+		'delete_published_posts' => true,
+		'edit_posts'             => true,
+		'delete_posts'           => true,
+		'read'                   => true,
+		'level_10'               => true,
+		'level_9'                => true,
+		'level_8'                => true,
+		'level_7'                => true,
+		'level_6'                => true,
+		'level_5'                => true,
+		'level_4'                => true,
+		'level_3'                => true,
+		'level_2'                => true,
+		'level_1'                => true,
+		'level_0'                => true,
 	);
 
 	/**
@@ -70,120 +145,6 @@ class WordPress_Sniffs_Theme_UseCapabilitiesNotRolesSniff extends WordPress_Abst
 		'author'        => true,
 		'contributor'   => true,
 		'subscriber'    => true,
-	);
-
-	/**
-	 * Array of core capabilities.
-	 *
-	 * @since 0.xx.0
-	 *
-	 * @var array Capabilities available in core.
-	 */
-	protected $core_capabilities = array(
-		'create_sites'           => true,
-		'delete_sites'           => true,
-		'manage_network'         => true,
-		'manage_sites'           => true,
-		'manage_network_users'   => true,
-		'manage_network_plugins' => true,
-		'manage_network_themes'  => true,
-		'manage_network_options' => true,
-		'create_sites'           => true,
-		'delete_sites'           => true,
-		'manage_network'         => true,
-		'manage_sites'           => true,
-		'manage_network_users'   => true,
-		'manage_network_plugins' => true,
-		'manage_network_themes'  => true,
-		'manage_network_options' => true,
-		'activate_plugins'       => true,
-		'delete_others_pages'    => true,
-		'delete_others_posts'    => true,
-		'delete_pages'           => true,
-		'delete_posts'           => true,
-		'delete_private_pages'   => true,
-		'delete_private_posts'   => true,
-		'delete_published_pages' => true,
-		'delete_published_posts' => true,
-		'edit_dashboard'         => true,
-		'edit_others_pages'      => true,
-		'edit_others_posts'      => true,
-		'edit_pages'             => true,
-		'edit_posts'             => true,
-		'edit_private_pages'     => true,
-		'edit_private_posts'     => true,
-		'edit_published_pages'   => true,
-		'edit_published_posts'   => true,
-		'edit_theme_options'     => true,
-		'export'                 => true,
-		'import'                 => true,
-		'list_users'             => true,
-		'manage_categories'      => true,
-		'manage_links'           => true,
-		'manage_options'         => true,
-		'moderate_comments'      => true,
-		'promote_users'          => true,
-		'publish_pages'          => true,
-		'publish_posts'          => true,
-		'read_private_pages'     => true,
-		'read_private_posts'     => true,
-		'read'                   => true,
-		'remove_users'           => true,
-		'switch_themes'          => true,
-		'upload_files'           => true,
-		'customize'              => true,
-		'delete_site'            => true,
-		'update_core'            => true,
-		'update_plugins'         => true,
-		'update_themes'          => true,
-		'install_plugins'        => true,
-		'install_themes'         => true,
-		'upload_plugins'         => true,
-		'upload_themes'          => true,
-		'delete_themes'          => true,
-		'delete_plugins'         => true,
-		'edit_plugins'           => true,
-		'edit_themes'            => true,
-		'edit_files'             => true,
-		'edit_users'             => true,
-		'create_users'           => true,
-		'delete_users'           => true,
-		'unfiltered_html'        => true,
-		'delete_others_pages'    => true,
-		'delete_others_posts'    => true,
-		'delete_pages'           => true,
-		'delete_posts'           => true,
-		'delete_private_pages'   => true,
-		'delete_private_posts'   => true,
-		'delete_published_pages' => true,
-		'delete_published_posts' => true,
-		'edit_others_pages'      => true,
-		'edit_others_posts'      => true,
-		'edit_pages'             => true,
-		'edit_posts'             => true,
-		'edit_private_pages'     => true,
-		'edit_private_posts'     => true,
-		'edit_published_pages'   => true,
-		'edit_published_posts'   => true,
-		'manage_categories'      => true,
-		'manage_links'           => true,
-		'moderate_comments'      => true,
-		'publish_pages'          => true,
-		'publish_posts'          => true,
-		'read'                   => true,
-		'read_private_pages'     => true,
-		'read_private_posts'     => true,
-		'upload_files'           => true,
-		'delete_posts'           => true,
-		'delete_published_posts' => true,
-		'edit_posts'             => true,
-		'edit_published_posts'   => true,
-		'publish_posts'          => true,
-		'read'                   => true,
-		'upload_files'           => true,
-		'delete_posts'           => true,
-		'edit_posts'             => true,
-		'read'                   => true,
 	);
 
 	/**
@@ -212,17 +173,23 @@ class WordPress_Sniffs_Theme_UseCapabilitiesNotRolesSniff extends WordPress_Abst
 
 		if ( isset( $this->core_roles[ $matched_parameter ] ) ) {
 			$this->phpcsFile->addError(
-				'Use capabilities and not roles in %s().',
+				'Capabilities should be used instead of roles. Found "%s" in function "%s"',
 				$stackPtr,
 				'RoleFound',
-				array( $matched_content )
+				array(
+					$matched_parameter,
+					$matched_content,
+				)
 			);
 		} else {
 			$this->phpcsFile->addWarning(
-				'The parameter %s is an unknown role or capability. Check %s() to ensure it is a capability and not a role.',
+				'"%s" is an unknown role or capability. Check the "%s()" function call to ensure it is a capability and not a role.',
 				$stackPtr,
-				'PossibleRoleFound',
-				array( $matched_parameter, $matched_content )
+				'UnknownCapabilityFound',
+				array(
+					$matched_parameter,
+					$matched_content
+				)
 			);
 		}
 
