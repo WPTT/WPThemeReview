de53e05c6d3e96dcb07236e405d8475e70407acd
jrfnl <github_nospam@adviesenzo.nl>
2017-04-09 02:41:12 +0200

    New sniff: Verify that an up-to-date version of TGMPA is being used.
    This sniff tries to find the TGM Plugin Activation library and if this is found, will check that:
    * the included class is up-to-date
    * isn't an unstable (alpha/beta) pre-release
    * if the `required_flavour` property is set, that the library was downloaded using the Custom TGMPA generator with the correct settings (warning)
    
    It also checks for a persistent manual search & replace error I have seen way too often which causes white screens of death (fatal error) for end-users.
    
    Includes extensive unit tests.

---
+++ WordPress/Tests/Theme/CorrectTGMPAVersionUnitTest.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Unit test class for WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+/**
+ * Unit test class for the Correct TGMPA Version sniff.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @since   0.xx.0
+ */
+class WordPress_Tests_Theme_CorrectTGMPAVersionUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Get a list of all test files to check.
+	 *
+	 * @param string $testFileBase The base path that the unit tests files will have.
+	 *
+	 * @return string[]
+	 */
+	protected function getTestFiles( $testFileBase ) {
+		$sep        = DIRECTORY_SEPARATOR;
+		$test_files = glob( dirname( $testFileBase ) . $sep . 'TGMPA' . $sep . 'CorrectVersionTests{ ' . $sep . ',' . $sep . '*' . $sep . '}*.inc', GLOB_BRACE );
+
+		if ( ! empty( $test_files ) ) {
+			return $test_files;
+		}
+
+		return array( $testFileBase . 'inc' );
+
+	} // End getTestFiles().
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @param string $testFile The name of the file being tested.
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList( $testFile = '' ) {
+
+		switch ( $testFile ) {
+			// Bad versions.
+			case 'class-tgm-plugin-activation-unstable.inc':
+			case 'prefixed-tgm-plugin-activation.inc':
+			case 'prefixed-tgm-plugin-activation.2.inc':
+			case 'manual-search-and-replace.inc':
+			case 'manual-search-and-replace.2.inc':
+				return array(
+					1 => 1,
+				);
+
+			// Official versions.
+			case 'class-tgm-plugin-activation-2.0.0.inc':
+			case 'class-tgm-plugin-activation-2.3.4.inc':
+			case 'class-tgm-plugin-activation-2.5.2.inc':
+				return array(
+					1 => 1,
+				);
+
+			// Renamed official versions.
+			case 'auto-install-1.0.0.inc':
+			case 'auto-install-1.1.0.inc':
+			case 'renamed-plugin-activation-2.0.0.inc':
+			case 'renamed-plugin-activation-2.1.0.inc':
+			case 'renamed-plugin-activation-2.1.1.inc':
+			case 'renamed-plugin-activation-2.2.0.inc':
+			case 'renamed-plugin-activation-2.2.1.inc':
+			case 'renamed-plugin-activation-2.2.2.inc':
+			case 'renamed-plugin-activation-2.3.0.inc':
+			case 'renamed-plugin-activation-2.3.1.inc':
+			case 'renamed-plugin-activation-2.3.2.inc':
+			case 'renamed-plugin-activation-2.3.3.inc':
+			case 'renamed-plugin-activation-2.3.4.inc':
+			case 'renamed-plugin-activation-2.3.5.inc':
+			case 'renamed-plugin-activation-2.3.6.inc':
+			case 'renamed-plugin-activation-2.4.0.inc':
+			case 'renamed-plugin-activation-2.4.1.inc':
+			case 'renamed-plugin-activation-2.4.2.inc':
+			case 'renamed-plugin-activation-2.5.0.inc':
+			case 'renamed-plugin-activation-2.5.1.inc':
+			case 'renamed-plugin-activation-2.5.2.inc':
+			case 'renamed-plugin-activation-2.6.0.inc':
+				return array(
+					1 => 1,
+				);
+
+			default:
+				return array();
+
+		} // End switch().
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @param string $testFile The name of the file being tested.
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList( $testFile = '' ) {
+		switch ( $testFile ) {
+			// Correct flavour check.
+			case 'tgmpa-2.6.1-themeforest-bad-official.inc':
+			case 'tgmpa-2.6.1-themeforest-bad-wporg.inc':
+			case 'tgmpa-2.6.1-wporg-bad-official.inc':
+			case 'tgmpa-2.6.1-wporg-bad-themeforest.inc':
+				return array(
+					1 => 1,
+				);
+
+			default:
+				return array();
+
+		} // End switch().
+
+	}
+
+} // End class.
