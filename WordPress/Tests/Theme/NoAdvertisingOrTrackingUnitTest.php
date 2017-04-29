ea1741613a1fe527a22969eac618eb4feab044ab
Carolina Nymark <myazalea@hotmail.com>
2017-04-29 09:45:40 +0200

    Unit tests
     Google search codes and Google advertising codes.

---
+++ WordPress/Tests/Theme/NoAdvertisingOrTrackingUnitTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Unit test class for WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+/**
+ * Unit test class for the NoAdvertisingOrTracking sniff.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @since   0.xx.0
+ */
+class WordPress_Tests_Theme_NoAdvertisingOrTrackingUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			3 => 1,
+			4 => 1,
+			6 => 1,
+			7 => 1,
+			11 => 1,
+			15 => 1,
+			17 => 1,
+			19 => 1,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array();
+
+	}
+
+} // End class.
