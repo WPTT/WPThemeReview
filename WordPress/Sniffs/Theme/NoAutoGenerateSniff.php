krishna19/WordPress-Coding-Standards

Update sniffs to use the new `PHP_CodeSniffer_Tokens::$textStringTokens` token array.

WordPress/Sniffs/Theme/NoAutoGenerateSniff.php
@@ -46,11 +46,8 @@ class WordPress_Sniffs_Theme_NoAutoGenerateSniff implements PHP_CodeSniffer_Snif
 	 * @return array
 	 */
 	public function register() {
-		$tokens   = PHP_CodeSniffer_Tokens::$stringTokens;
-		$tokens[] = T_INLINE_HTML;
-		$tokens[] = T_HEREDOC;
+		$tokens   = PHP_CodeSniffer_Tokens::$textStringTokens;
 		$tokens[] = T_STRING; // Functions named after or prefixed with the generator name.
-		$tokens[] = T_NOWDOC;
 		$tokens[] = T_COMMENT;
 		$tokens[] = T_DOC_COMMENT_STRING;
 		return $tokens;

WordPress/Sniffs/Theme/NoFaviconSniff.php
@@ -83,12 +83,7 @@ class WordPress_Sniffs_Theme_NoFaviconSniff implements PHP_CodeSniffer_Sniff {
 
 		$this->favicon_regex = sprintf( self::REGEX_TEMPLATE, implode( '|', $regex_parts ) );
 
-		$tokens   = PHP_CodeSniffer_Tokens::$stringTokens;
-		$tokens[] = T_INLINE_HTML;
-		$tokens[] = T_HEREDOC;
-		$tokens[] = T_NOWDOC;
-
-		return $tokens;
+		return PHP_CodeSniffer_Tokens::$textStringTokens;
 	}
 
 	/**

WordPress/Sniffs/Theme/NoTitleTagSniff.php
@@ -31,12 +31,7 @@ class WordPress_Sniffs_Theme_NoTitleTagSniff extends WordPress_Sniff {
 	 * @return array
 	 */
 	public function register() {
-		$tokens                  = PHP_CodeSniffer_Tokens::$stringTokens;
-		$tokens[ T_INLINE_HTML ] = T_INLINE_HTML;
-		$tokens[ T_HEREDOC ]     = T_HEREDOC;
-		$tokens[ T_NOWDOC ]      = T_NOWDOC;
-
-		return $tokens;
+		return PHP_CodeSniffer_Tokens::$textStringTokens;
 	}
 
 	/**

WPTT/WPThemeReview

WordPress/Sniffs/Theme/NoAutoGenerateSniff.php
@@ -51,11 +51,8 @@ class NoAutoGenerateSniff extends Sniff {
 	 * @return array
 	 */
 	public function register() {
-		$tokens   = Tokens::$stringTokens;
-		$tokens[] = T_INLINE_HTML;
-		$tokens[] = T_HEREDOC;
+		$tokens   = Tokens::$textStringTokens;
 		$tokens[] = T_STRING; // Functions named after or prefixed with the generator name.
-		$tokens[] = T_NOWDOC;
 		$tokens[] = T_COMMENT;
 		$tokens[] = T_DOC_COMMENT_STRING;
 		return $tokens;

WordPress/Sniffs/Theme/NoFaviconSniff.php
@@ -88,12 +88,7 @@ class NoFaviconSniff extends Sniff {
 
 		$this->favicon_regex = sprintf( self::REGEX_TEMPLATE, implode( '|', $regex_parts ) );
 
-		$tokens   = Tokens::$stringTokens;
-		$tokens[] = T_INLINE_HTML;
-		$tokens[] = T_HEREDOC;
-		$tokens[] = T_NOWDOC;
-
-		return $tokens;
+		return Tokens::$textStringTokens;
 	}
 
 	/**

WordPress/Sniffs/Theme/NoTitleTagSniff.php
@@ -36,12 +36,7 @@ class NoTitleTagSniff extends Sniff {
 	 * @return array
 	 */
 	public function register() {
-		$tokens                  = Tokens::$stringTokens;
-		$tokens[ T_INLINE_HTML ] = T_INLINE_HTML;
-		$tokens[ T_HEREDOC ]     = T_HEREDOC;
-		$tokens[ T_NOWDOC ]      = T_NOWDOC;
-
-		return $tokens;
+		return Tokens::$textStringTokens;
 	}
 
 	/**