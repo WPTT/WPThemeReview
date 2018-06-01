99c058835f06656d681a0b8ee54ceb99eb40ba7c
jrfnl <jrfnl@users.noreply.github.com>
2018-06-02 01:00:30 +0200

    NoDeregisterCoreScript: Update for PHPCS 3.x compatibility

--- WordPress/Tests/Theme/NoDeregisterCoreScriptUnitTest.php
+++ WordPress/Tests/Theme/NoDeregisterCoreScriptUnitTest.php
@@ -7,13 +7,17 @@
  * @license https://opensource.org/licenses/MIT MIT
  */
 
+namespace WordPress\Tests\Theme;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
 /**
  * Unit test class for the NoDeregisterCoreScript sniff.
  *
  * @package WPCS\WordPressCodingStandards
  * @since   0.xx.0
  */
-class WordPress_Tests_Theme_NoDeregisterCoreScriptUnitTest extends AbstractSniffUnitTest {
+class NoDeregisterCoreScriptUnitTest extends AbstractSniffUnitTest {
 
 	/**
 	 * Returns the lines where errors should occur.
@@ -38,4 +42,4 @@ class WordPress_Tests_Theme_NoDeregisterCoreScriptUnitTest extends AbstractSniff
 		return array();
 	}
 
-} // End Class.
+}
