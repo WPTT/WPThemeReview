edba3f2d582340f939b50a7ae312f5109066215f
jrfnl <jrfnl@users.noreply.github.com>
2018-09-15 04:15:16 +0200

    :scissors: Tests: adjust the PHPUnit bootstrap and config file
    PHPUnit bootstrap:
    * Remove work-arounds for supporting low PHPCS 3.x versions.
    * Remove references to supporting PHPCS < 3.
    * Include the WPThemeReview autoload file rather than the WP one.
    * Minor code optimization.
    * Update File docblock for the bootstrap file being in the WPThemeReview standard, including updating the file changelog.
    * Include additional inline documentation about how things work.
    
    PHPUnit config file:
    * Document which tests will be run.
        This isn't strictly needed as PHPCS sorts this out, but for documentation purposes, it doesn't hurt.
    * As devs are expected to have PHPCompatibility installed as well, they may run into issues running the unit tests. By setting a special environment variable, these issues are circumvented.

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -1,49 +1,61 @@
 <?php
 /**
- * Bootstrap file for running the tests on PHPCS 3.x.
+ * Bootstrap file for running the tests.
  *
- * Load the PHPCS autoloader and the WPCS PHPCS cross-version helpers.
+ * - Load the PHPCS PHPUnit bootstrap file providing cross-version PHPUnit support.
+ *   {@link https://github.com/squizlabs/PHP_CodeSniffer/pull/1384}
+ * - Load the WPThemeReview autoload file.
+ *   This file loads the WPCS alias file which is needed as several WPThemeReview
+ *   sniffs extend WPCS sniffs.
  *
- * {@internal We need to load the PHPCS autoloader first, so as to allow their
- * auto-loader to find the classes we want to alias for PHPCS 3.x.
+ * {@internal The PHPCS autoloader needs to be loaded first, so as to allow their
+ * auto-loader to find the classes WPCS aliases for PHPCS 3.x.
  * This aliasing has to be done before any of the test classes are loaded by the
- * PHPCS native unit test suite to prevent fatal errors.
+ * PHPCS native unit test suite to prevent fatal errors.}}
  *
- * @package WPCS\WordPressCodingStandards
- * @link    https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+ * @package WPTRT\WPThemeReview
+ * @link    https://github.com/WPTRT/WPThemeReview
  * @license https://opensource.org/licenses/MIT MIT
- * @since   0.13.0
+ *
+ * @since   WPCS 0.13.0
+ * @since   TRTCS 0.1.0 Adjusted for use in the WPThemeReview standard.
  */
 
 if ( ! \defined( 'PHP_CODESNIFFER_IN_TESTS' ) ) {
 	define( 'PHP_CODESNIFFER_IN_TESTS', true );
 }
 
-$ds = DIRECTORY_SEPARATOR;
+$ds          = DIRECTORY_SEPARATOR;
+$projectRoot = dirname( __DIR__ ) . $ds;
 
+/*
+ * Load the necessary PHPCS files.
+ */
 // Get the PHPCS dir from an environment variable.
-$phpcsDir = getenv( 'PHPCS_DIR' );
+$phpcsDir          = getenv( 'PHPCS_DIR' );
+$composerPHPCSPath = $projectRoot . 'vendor' . $ds . 'squizlabs' . $ds . 'php_codesniffer';
 
-// This may be a Composer install.
-if ( false === $phpcsDir && is_dir( dirname( __DIR__ ) . $ds . 'vendor' . $ds . 'squizlabs' . $ds . 'php_codesniffer' ) ) {
-	$phpcsDir = dirname( __DIR__ ) . $ds . 'vendor' . $ds . 'squizlabs' . $ds . 'php_codesniffer';
+if ( false === $phpcsDir && is_dir( $composerPHPCSPath ) ) {
+	// PHPCS installed via Composer.
+	$phpcsDir = $composerPHPCSPath;
 } elseif ( false !== $phpcsDir ) {
+	/*
+	 * PHPCS in a custom directory.
+	 * For this to work, the `PHPCS_DIR` needs to be set in a custom `phpunit.xml` file.
+	 */
 	$phpcsDir = realpath( $phpcsDir );
 }
 
 // Try and load the PHPCS autoloader.
-if ( false !== $phpcsDir && file_exists( $phpcsDir . $ds . 'autoload.php' ) ) {
+if ( false !== $phpcsDir
+	&& file_exists( $phpcsDir . $ds . 'autoload.php' )
+	&& file_exists( $phpcsDir . $ds . 'tests' . $ds . 'bootstrap.php' )
+) {
 	require_once $phpcsDir . $ds . 'autoload.php';
+	require_once $phpcsDir . $ds . 'tests' . $ds . 'bootstrap.php'; // PHPUnit 6.x+ support.
 
-	/*
-	 * As of PHPCS 3.1, PHPCS supports PHPUnit 6.x, but needs a bootstrap, so
-	 * load it if it's available.
-	 */
-	if ( file_exists( $phpcsDir . $ds . 'tests' . $ds . 'bootstrap.php' ) ) {
-		require_once $phpcsDir . $ds . 'tests' . $ds . 'bootstrap.php';
-	}
 } else {
-	echo 'Uh oh... can\'t find PHPCS. Are you sure you are using PHPCS 3.x ?
+	echo 'Uh oh... can\'t find PHPCS.
 
 If you use Composer, please run `composer install`.
 Otherwise, make sure you set a `PHPCS_DIR` environment variable in your phpunit.xml file
@@ -56,6 +68,10 @@ https://is.gd/contributing2WPCS
 	die( 1 );
 }
 
-// Load our class aliases.
-require_once dirname( __DIR__ ) . $ds . 'WordPress' . $ds . 'PHPCSAliases.php';
-unset( $ds, $phpcsDir );
+/*
+ * Load the WPThemeReview autoload file.
+ */
+require_once $projectRoot . 'autoload.php';
+
+// Clean up.
+unset( $ds, $projectRoot, $phpcsDir, $composerPHPCSPath );
