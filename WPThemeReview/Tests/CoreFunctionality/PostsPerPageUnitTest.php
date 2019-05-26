d680d71b99cd38a7aed0a926eebb08a82e727b88
Denis Žoljom <denis.zoljom@gmail.com>
2019-05-26 09:15:33 +0200

    Extend the ppp sniff and add -1 warning
    Added tests for this and removed the unnecessary ruleset addition.

---
+++ WPThemeReview/Tests/CoreFunctionality/PostsPerPageUnitTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Unit test class for WPThemeReview Coding Standard.
+ *
+ * @package WPTRT\WPThemeReview
+ * @link    https://github.com/WPTRT/WPThemeReview
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+namespace WPThemeReview\Tests\CoreFunctionality;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the PostsPerPage sniff.
+ *
+ * @package WPTRT\WPThemeReview
+ *
+ * @since 0.2.0
+ */
+class PostsPerPageUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array();
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array(
+			3  => 1,
+			4  => 1,
+			7  => 1,
+			9  => 1,
+			10 => 1,
+			11 => 1,
+			12 => 1,
+			13 => 1,
+			16 => 1,
+			19 => 1,
+			21 => 1,
+			22 => 1,
+			23 => 1,
+			25 => 1,
+			29 => 1,
+		);
+	}
+
+}
